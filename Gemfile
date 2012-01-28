344c7220ba4370b4924b35f0864686c495e9af6e
KamaKAzii <spskamakazii@gmail.com>
2012-01-28 16:57:36 +1100

    Initial commit using staticmatic-bootstrap

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'staticmatic', '~> 0.11.1'
+gem 'haml', '~> 3.1.1'
+gem 'sass', '~> 3.1.1'
+gem 'compass', '~> 0.11.1'
+gem 'css_parser', '~> 1.1.9'
+gem 'maruku', '~> 0.6.0'
---
+++ config.rb
@@ -0,0 +1,25 @@
+# Compass Configuration
+# Learn more: http://compass-style.org/docs/tutorials/configuration-reference/
+
+# HTTP paths
+http_path             = '/'
+http_stylesheets_path = '/stylesheets'
+http_images_path      = '/images'
+http_javascripts_path = '/javascripts'
+
+# File system locations
+sass_dir              = 'src/stylesheets'
+css_dir               = 'site/stylesheets'
+images_dir            = 'site/images'
+javascripts_dir       = 'site/javascripts'
+
+# Set to true for easier debugging
+line_comments         = false
+preferred_syntax      = :sass
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style          = :expanded
+
+# Determine whether Compass asset helper functions generate relative
+# or absolute paths
+relative_assets       = true
