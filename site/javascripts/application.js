df7657cb9089d3a25414c38dbb555aabe15a9773
KamaKAzii <spskamakazii@gmail.com>
2012-02-10 13:09:27 +1100

    Add styles for content inside articles. Accounts for both single and side by side panel layout

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -23,3 +23,35 @@ $("nav div").hover(
     $(this).animate({ top: "0" }, { queue: false, duration: 300 });
   }
 );
+
+// Carousel
+
+var carousel = {
+  utils: function() {
+    alert("hello");
+  },
+  articleWidth: 730,
+  articleLeftMargin: 15
+};
+
+// LOL this is the funniest javascript style in the history of mankind...
+// Attach the numbers of each article to a corresponding button and also
+// get the total number of panels while you're at it...
+
+carousel.total = $("nav ul li").each(function(index) {
+  $(this).data("articleIndex", index);
+}).length;
+
+$("nav ul li").click(function() {
+  // Get the location you wish it the section to animate to
+  carousel.targetPosition =
+    $(this).data("articleIndex") * (carousel.articleWidth + carousel.articleLeftMargin) - carousel.articleLeftMargin;
+  // Animate the m0f0!
+  $("section").animate(
+    { left: -carousel.targetPosition },
+    {
+      duration: 1000,
+      queue: false
+    }
+  );
+});
